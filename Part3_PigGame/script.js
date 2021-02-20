a61580e1bf9ae75999a3ed3075c11f3630e564b4
ChristopherCaysido <42668379+ChristopherCaysido@users.noreply.github.com>
2021-02-20 12:10:22 +0800

    Dom Manip

--- Part3_PigGame/script.js
+++ Part3_PigGame/script.js
@@ -1,7 +1,9 @@
-'use strict';
+
 var scores, roundScore;
 scores = [0,0];
 roundScore = 0;
-currentPlayer = 0;
+activePlayer = 1;
 
-dice = Math.floor(Math.random())
+dice = Math.floor(Math.random()*6) + 1;
+document.querySelector('#current--' + activePlayer).textContent = dice;
+// document.querySelector('#current--' + activePlayer).innerHTML = '<em>'dice'</em>';
