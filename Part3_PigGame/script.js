4b67ed4e1724dd3c318478b9f53b1b8c68d811d3
ChristopherCaysido <42668379+ChristopherCaysido@users.noreply.github.com>
2021-02-22 02:38:22 +0800

    Update Current Score

--- Part3_PigGame/script.js
+++ Part3_PigGame/script.js
@@ -2,27 +2,29 @@
 var scores, roundScore;
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
-c
+activePlayer = 0;
+
 // dice = Math.floor(Math.random()*6) + 1;
 // var displayRoll = document.querySelector('#current--' + activePlayer).textContent = dice;
 // // document.querySelector('#current--' + activePlayer).innerHTML = '<em>'dice'</em>';
 
 document.querySelector('.dice').style.display = 'none'; 
-scoreDOM = document.getElementById('score-'+ activePlayer);
-currentDOM = document.getElementById('current-'+activePlayer);
-for (i = 0; i < 2; i = i + 1){
-    activePlayer = i;
-    currentDOM
-}
+
 
 document.querySelector('.btn--roll').addEventListener('click',function(){
     // Roll a Random Number
-    dice = Math.floor(Math.random()*6) + 1;
+    var dice = Math.floor(Math.random()*6) + 1;
     // Display the Number
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     // Update the round score if the rolled number was NOT a 1
-    
-})
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current--'+activePlayer).textContent = roundScore;
+    } else {
+        //Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    }
+});
