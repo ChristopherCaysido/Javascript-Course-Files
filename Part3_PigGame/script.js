d01b19c113a8c0b06448119aa1b877a6d8801a4d
ChristopherCaysido <42668379+ChristopherCaysido@users.noreply.github.com>
2021-03-01 12:54:54 +0800

    Added functionality

--- Part3_PigGame/script.js
+++ Part3_PigGame/script.js
@@ -1,5 +1,5 @@
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 initFunction();
 // dice = Math.floor(Math.random()*6) + 1;
@@ -15,24 +15,29 @@ document.querySelector('.dice').style.display = 'none';
 document.querySelector('.btn--new').addEventListener('click',initFunction)
 
 document.querySelector('.btn--roll').addEventListener('click',function(){
+    if (gamePlaying){
+
+    
     // Roll a Random Number
-    var dice = Math.floor(Math.random()*6) + 1;
-    // Display the Number
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
-    // Update the round score if the rolled number was NOT a 1
-    if (dice !== 1) {
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current--'+activePlayer).textContent = roundScore;
-    } else {
-        //Next Player
-        document.querySelector('#current--'+activePlayer).textContent = 0;
-        roundScore = 0;
-        nextPlayer();
-        
+        var dice = Math.floor(Math.random()*6) + 1;
+        // Display the Number
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        // Update the round score if the rolled number was NOT a 1
+        if (dice !== 1) {
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current--'+activePlayer).textContent = roundScore;
+        } else {
+            //Next Player
+            document.querySelector('#current--'+activePlayer).textContent = 0;
+            roundScore = 0;
+            nextPlayer();    
     }
+
+}
+
 });
 
 document.querySelector('.btn--hold').addEventListener('click',function(){
@@ -43,6 +48,7 @@ document.querySelector('.btn--hold').addEventListener('click',function(){
     //Pass it to other player
     if(scores[activePlayer] >= 100){
         document.querySelector('#name--'+activePlayer).textContent = 'Winner';
+        gamePlaying === false;
     } else {
         document.querySelector('#current--'+activePlayer).textContent = 0;
         roundScore = 0;
@@ -55,6 +61,7 @@ function initFunction(){
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
     document.getElementById('score--0').textContent = 0;
     document.getElementById('score--1').textContent = 0;
     document.getElementById('current--0').textContent = 0;
